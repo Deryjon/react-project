c8d061a29eb1badf047ef510d003c1fa3b99cd59
Dery Jon <yusupoviskandar1005@gmail.com>
2024-05-31 15:23:21 +0500

    finish: finished hero section (2024-05-31, 15:23)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 const  App = () => {
   return (
-    <><Navbar /><HeroSection /></>
+    <div className="max-w-7xl mx-auto pt-10 px-6">
+    <Navbar /><HeroSection /></div>
   );
 };
 export default  App
